259beb224a15033c57aee2c4968261c77b5ea9d8
Sara Nolan <saraa.ann28@gmail.com>
2021-03-09 16:43:20 -0500

    added schemas.sql

---
+++ schemas.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS employees_db;
+
+CREATE DATABASE employees_db;
+
+USE employees_db;
+
+CREATE TABLE employee (
+	id INT AUTO_INCREMENT NOT NULL,
+    first_name varchar(30) NOT NULL,
+    last_name varchar(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL,
+    primary key(id)
+);
+
+CREATE TABLE role (
+	id INT AUTO_INCREMENT NOT NULL,
+    title varchar(30) NOT NULL,
+    salary DECIMAL(10,2) NULL,
+    department_id INT NOT NULL,
+    primary key(id)
+);
+
+CREATE TABLE department (
+	id INT AUTO_INCREMENT NOT NULL,
+    name VARCHAR(30) NOT NULL,
+    primary key(id)
+    );
+
